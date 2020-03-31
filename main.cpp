17f8786f8fa16351d39ffe1cb211adeecaef1618
ellisrourkePC <ellisrourke@gmail.com>
2020-03-31 20:56:55 +1000

    added rustyQueue to handle pairs

--- main.cpp
+++ main.cpp
@@ -3,11 +3,6 @@
 #include <ctime>
 #include <vector>
 
-void dispVect(std::vector<int> & arr){
-    for(int i=0;i<arr.size();i++){
-        std::cout<<arr[i] << " ";
-    }std::cout << std::endl;
-}
 class table{
 public:
     table(){
@@ -16,18 +11,14 @@ public:
     static int coinFlip(){
         return rand() % 2;
     }
-    static int sumOfDigits(std::vector<int> const & nums){
-        int n,m, sum = 0;
-        for(int i=0;i<nums.size();i++){
-            n = nums[i];
+    static int sumOfDigits(int n){
+            int sum = 0,m = 0;
             while(n>0){
                 m = n%10;
                 sum+=m;
                 n = n/10;
             }
-            std::cout << sum << std::endl;
-            sum=0;
-        }
+        return sum;
     }
     int rustyMove(){
         //chose ball with highest sum of digits
@@ -44,16 +35,18 @@ class priorityQueue{
 public:
     priorityQueue()= default;;
     bool isEmpty(){ return capacity==0; }
-    int getMax(){ return queue[1]; }
-    void insertElement(int value){
+
+    virtual int getMax(){ return queue[1]; }
+    virtual void insertElement(int value){
         if(capacity + 1 <= queue.size()){
             queue.push_back(0);
         }
         queue[++capacity] = value;
         shiftUp(capacity);
-        dispVect(queue);
+        dispQueue(queue);
     }
-    void shiftUp(int index){
+
+    virtual void shiftUp(int index){
         if(index > capacity){ return; } //recursive base case
         if(index == 1){ return; } //recursive base case
         if(queue[index] > queue[parent(index)]){ //if element is greater value than its parent, swap
@@ -61,7 +54,8 @@ public:
         }
         shiftUp(parent(index)); //recurse until correct position is found
     }
-    void shiftDown(int index){
+
+    virtual void shiftDown(int index){
         if(index > capacity){ return; }
         int swapId = index;
 
@@ -76,14 +70,20 @@ public:
             shiftDown(swapId);
         }
     }
-    void removeMax(){
+
+    virtual void removeMax(){
         int value = queue[1];
         std::swap(queue[1],queue[capacity]);
         capacity--;
         shiftDown(1);
 
     };
-private:
+    static void dispQueue(std::vector<int> & arr){
+        for(int i=0;i<arr.size();i++){
+            std::cout<<arr[i] << " ";
+        }std::cout << std::endl;
+    }
+protected:
     std::vector<int> queue{-1};
     int capacity{};
     static int parent(int const& index){ return (index/2); }
@@ -91,6 +91,55 @@ private:
     static int rightChild(int const& index){ return (index*2)+1 ; }
 };
 
+class rustyQueue : public priorityQueue{ //<ball no, sum of digits
+public:
+    int getMax(){ return queue[1].second; }
+    void insertElement(int value) override {
+        if(capacity + 1 <= queue.size()){
+            queue.emplace_back(queue.size(),0);
+        }
+        queue[++capacity] = std::make_pair(queue.size()-1,value);
+        shiftUp(capacity);
+        dispQueue(queue);
+    }
+    void shiftUp(int index) override{
+        if(index > capacity){ return; } //recursive base case
+        if(index == 1){ return; } //recursive base case
+        if(queue[index] > queue[parent(index)]){ //if element is greater value than its parent, swap
+            std::swap(queue[parent(index)],queue[index]); //swap positions to arrange
+        }
+        shiftUp(parent(index)); //recurse until correct position is found
+    }
+    void shiftDown(int index) override{
+        if(index > capacity){ return; }
+        int swapId = index;
+
+        if(leftChild(index) <= capacity && queue[index] < queue[leftChild(index)]){
+            swapId = leftChild(index); //left child is greater than parent, so swap
+        }
+        if(rightChild(index) <= capacity && queue[swapId] < queue[rightChild(index)]){
+            swapId = rightChild(index); //right child grater than parent and left child
+        }
+        if(swapId != index){ //recursive base case
+            std::swap(queue[index],queue[swapId]);
+            shiftDown(swapId);
+        }
+    }
+    void removeMax() override {
+        int value = queue[1].second;
+        std::swap(queue[1],queue[capacity]);
+        capacity--;
+        shiftDown(1);
+
+    };
+    static void dispQueue(std::vector<std::pair<int,int>>& arr){
+        for(int i=0;i<arr.size();i++){
+            std::cout<< "(" << arr[i].first << " , " <<arr[i].second << ")";
+        }std::cout << std::endl;
+    }
+private:
+    std::vector<std::pair<int,int>> queue {std::make_pair(-1,-1)};
+};
 
 int main(int argc, char *argv[]) {
     srand(time(nullptr));
@@ -99,16 +148,14 @@ int main(int argc, char *argv[]) {
     std::vector<int> myNumbers = {22, 97, 123}; // 4 16 6
 
     priorityQueue maxHeap = *new priorityQueue;
-    maxHeap.insertElement(1);
-    maxHeap.insertElement(3);
-    maxHeap.insertElement(4);
-    maxHeap.insertElement(6);
-    maxHeap.insertElement(13);
-    maxHeap.insertElement(10);
-    maxHeap.insertElement(9);
-    maxHeap.insertElement(8);
-    maxHeap.insertElement(15);
-    maxHeap.insertElement(17);
+    priorityQueue scottValues = *new priorityQueue;
+    rustyQueue rustyValues = *new rustyQueue;
+    rustyValues.insertElement(table::sumOfDigits(11));
+    rustyValues.insertElement(table::sumOfDigits(1122));
+    rustyValues.insertElement(table::sumOfDigits(123));    ;
+    rustyValues.insertElement(table::sumOfDigits(1234));
+    //std::cout << rustyValues.getMax();
+
 }
 //1 3 4 6 13 10 9 8 15 17
 //1 ,3 ,4 ,6 ,13 ,10 ,9 ,8 ,15 ,17
