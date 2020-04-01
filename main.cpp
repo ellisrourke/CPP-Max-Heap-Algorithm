91e0419a12c95506d0e8fb60e4f496a9394914a6
ellisrourkePC <ellisrourke@gmail.com>
2020-04-01 14:47:17 +1000

    working priority queues for both scott and rusty depending on their preference

--- main.cpp
+++ main.cpp
@@ -26,13 +26,13 @@ public:
         //chose highest value ball
     }
     */
-private:
-
 };
 
 class priorityQueue{
 public:
-    priorityQueue()= default;;
+    explicit priorityQueue(std::vector<int> *usedBalls){
+        this->usedBalls = usedBalls;
+    }
     bool isEmpty(){ return capacity==0; }
 
     virtual int getMax(){ return queue[1].second; }
@@ -48,7 +48,7 @@ public:
     virtual void shiftUp(int index){
         if(index > capacity){ return; } //recursive base case
         if(index == 1){ return; } //recursive base case
-        if(queue[index] > queue[parent(index)]){ //if element is greater value than its parent, swap
+        if(queue[index].second > queue[parent(index)].second){ //if element is greater value than its parent, swap
             std::swap(queue[parent(index)],queue[index]); //swap positions to arrange
         }
         shiftUp(parent(index)); //recurse until correct position is found
@@ -58,32 +58,46 @@ public:
         if(index > capacity){ return; }
         int swapId = index;
 
-        if(leftChild(index) <= capacity && queue[index] < queue[leftChild(index)]){
+        if(leftChild(index) <= capacity && queue[index].second < queue[leftChild(index)].second){
             swapId = leftChild(index); //left child is greater than parent, so swap
         }
-        if(rightChild(index) <= capacity && queue[swapId] < queue[rightChild(index)]){
+        if(rightChild(index) <= capacity && queue[swapId].second < queue[rightChild(index)].second){
             swapId = rightChild(index); //right child grater than parent and left child
         }
         if(swapId != index){ //recursive base case
             std::swap(queue[index],queue[swapId]);
             shiftDown(swapId);
         }
+
     }
 
     virtual void removeMax(){
         int value = queue[1].second;
-        std::swap(queue[1],queue[capacity]);
-        capacity--;
+        std::cout << std::endl << value << std::endl;
+        updateUsedBalls(queue[1].first);
+        std::swap(queue[1],queue[capacity--]);
         shiftDown(1);
-
+        //capacity--;
     };
 
     virtual void dispQueue(){
-        for(int i=0;i<this->queue.size();i++){
+        for(int i=0;i<capacity+1;i++){
             std::cout<< "(" <<this->queue[i].first << " , " << this->queue[i].second << ")";
         }std::cout << std::endl;
     }
+    std::vector<int> *usedBalls;
+
+    void updateUsedBalls(int x){ //ball ID
+        usedBalls->push_back(x);
+    }
+    void displayUsedBalls(){
+        int* index;
+        for(int i=0;i<this->usedBalls->size();i++){
+            std::cout << (*usedBalls)[i] << " ";
+        } std::cout << std::endl;
+    }
 protected:
+
     std::vector<std::pair<int,int>> queue{std::make_pair(-1,-1)};
     int capacity{};
     static int parent(int const& index){ return (index/2); }
@@ -93,6 +107,7 @@ protected:
 
 class rustyQueue : public priorityQueue{ //<ball no,value, sum of digits>
 public:
+    using priorityQueue::priorityQueue;
     int getMax() override{ return std::get<2>(queue[1]); }
     void insertElement(int value) override {
         if(capacity + 1 <= queue.size()){
@@ -105,7 +120,7 @@ public:
     void shiftUp(int index) override{
         if(index > capacity){ return; } //recursive base case
         if(index == 1){ return; } //recursive base case
-        if(queue[index] > queue[parent(index)]){ //if element is greater value than its parent, swap
+        if(std::get<2>(queue[index]) > std::get<2>(queue[parent(index)])){ //if element is greater value than its parent, swap
             std::swap(queue[parent(index)],queue[index]); //swap positions to arrange
         }
         shiftUp(parent(index)); //recurse until correct position is found
@@ -114,10 +129,10 @@ public:
         if(index > capacity){ return; }
         int swapId = index;
 
-        if(leftChild(index) <= capacity && queue[index] < queue[leftChild(index)]){
+        if(leftChild(index) <= capacity && std::get<2>(queue[index]) < std::get<2>(queue[leftChild(index)])){
             swapId = leftChild(index); //left child is greater than parent, so swap
         }
-        if(rightChild(index) <= capacity && queue[swapId] < queue[rightChild(index)]){
+        if(rightChild(index) <= capacity && std::get<2>(queue[swapId]) < std::get<2>(queue[rightChild(index)])){
             swapId = rightChild(index); //right child grater than parent and left child
         }
         if(swapId != index){ //recursive base case
@@ -127,17 +142,20 @@ public:
     }
     void removeMax() override {
         int value = std::get<2>(queue[1]);
+        updateUsedBalls(std::get<0>(queue[1]));
         std::swap(queue[1],queue[capacity]);
         capacity--;
         shiftDown(1);
 
     };
     void dispQueue() override{
-        for(int i=0;i<this->queue.size();i++){
+        for(int i=0;i<capacity+1;i++){
             std::cout<< "(" << std::get<0>(this->queue[i]) << " , " <<std::get<1>(this->queue[i]) << " , " << std::get<2>(this->queue[i]) << ")";
         }std::cout << std::endl;
     }
+    std::vector<int> usedBalls;
 private:
+    //std::vector<int> usedBalls;
     std::vector<std::tuple<int,int,int>> queue {std::make_tuple(-1,-1,-1)};
 };
 
@@ -146,26 +164,25 @@ int main(int argc, char *argv[]) {
     int k = 0; //number of turns per round
     int n = 0; //number of balls on table
     std::vector<int> usedBalls = {};
-
-    priorityQueue scottValues = *new priorityQueue;
-    rustyQueue rustyValues = *new rustyQueue;
-    std::cout << "--- Rusty ---" << std::endl;
-    rustyValues.insertElement(11);
-    rustyValues.dispQueue();
-    rustyValues.insertElement(23);
-    rustyValues.dispQueue();
-    std::cout << rustyValues.getMax();
-
-    std::cout << std::endl << "--- Scott ---" << std::endl;
+    priorityQueue scottValues = *new priorityQueue(&usedBalls);
+    rustyQueue rustyValues = *new rustyQueue(&usedBalls);
+    scottValues.insertElement(12);
+    scottValues.insertElement(54);
     scottValues.insertElement(11);
     scottValues.dispQueue();
-    scottValues.insertElement(23);
+    scottValues.removeMax();
     scottValues.dispQueue();
-    std::cout << scottValues.getMax();
+    scottValues.removeMax();
+    scottValues.dispQueue();
+    //scottValues.update(12);
 
+    rustyValues.displayUsedBalls();
+    //sample input
+    //2 - number of test cases
+    //3 2 - n & k where n =n number of balls, k = number of turns
+    //1000 99 98 - the balls
+    //TAILS - result of coin flip(T = Rusty, H = Scott)
 
-
-    //scottValues.insertElement(11);
 }
 //1 3 4 6 13 10 9 8 15 17
 //1 ,3 ,4 ,6 ,13 ,10 ,9 ,8 ,15 ,17
