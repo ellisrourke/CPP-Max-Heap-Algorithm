c912bb7cbedbdfb60d5b582dbf2f437b660b5bb3
ellisrourkePC <ellisrourke@gmail.com>
2020-04-09 17:08:28 +1000

    very close logic

--- main.cpp
+++ main.cpp
@@ -134,22 +134,23 @@ private:
     std::vector<std::tuple<int,int,int>> queue {std::make_tuple(-1,-1,-1)};
 };
 
-
 int main(int argc, char *argv[]) {
     srand(time(nullptr));
-    int n = 20; //number of balls on table
-    int k = 1; //number of turns per round
-    bool flip = false; //were 1 = heads and 0 = tails // tails = rusty , heads = scott
+    int n = 8; //number of balls on table
+    int k = 2; //number of turns per round
+    bool flip = true; //were 1 = heads and 0 = tails // tails = rusty , heads = scott
     std::vector<int> usedBalls = {};
     std::unordered_map<int, bool> used;
-    std::vector<int> initialBalls = {76 ,89,49 ,2 ,31, 311 ,56 ,445, 343 ,100 ,900 ,111 ,323 ,232, 32 ,35 ,6456, 565 ,760, 878};
-    //std::vector<int> initialBalls = {50,60,20,10,90,30,60,70};
+    //std::vector<int> initialBalls = {76 ,89,49 ,2 ,31, 311 ,56 ,445, 343 ,100 ,900 ,111 ,323 ,232, 32 ,35 ,6456, 565 ,760, 878};
+    std::vector<int> initialBalls = {50,60,20,10,90,30,60,70};
     //std::vector<int> initialBalls = {65,8,74,97,61,19,75,52,85,80,96,82,26,52,50,67,99,27,42,61,45,67,61,52,21,13,31,16,5,50,65,53,24,65,62,51,34,43,82,68,40,1,73,63,88,44,47,6,34,57,11,51,86,55,38,25,72,94,18,92,98,65,1,16,75,61,2,92,64,63,27,17,28,75,33};
     priorityQueue scottValues = *new priorityQueue(&usedBalls);
     rustyQueue rustyValues = *new rustyQueue(&usedBalls);
     int scottScore = 0;
     int rustyScore = 0;
     int totalTurns =0;
+    int rustyTurns =0;
+    int scottTurns = 0;
     for (int i = 0; i < n; i++) {
         rustyValues.insertElement(initialBalls[i]);
         scottValues.insertElement(initialBalls[i]);
@@ -157,40 +158,46 @@ int main(int argc, char *argv[]) {
     }
 
     while(totalTurns < n)  {
+
         if (flip) { //scott turn
             int turn = 0;
             while(turn < k){
+                if(scottValues.getMax() == -1){break;}
 
                 int value = scottValues.getMax();
+
                 int index = scottValues.getMaxIndex();
-                std::cout << value << " " << index << std::endl;
+                //std::cout << value << " " << index << std::endl;
                 scottValues.removeMax();
                 //if(!(std::find(usedBalls.begin(), usedBalls.end(), index) != usedBalls.end())){
                 if(!used[index]){
                     scottScore += value;
+                    std::cout << "turn " << turn << " scott choses " << value <<std::endl;
                     turn++;
-                    totalTurns+=1;
+                    totalTurns++;
                     used[index] = true;
                 }
-                if(scottValues.isEmpty()){break;}
             }
         }
         else if(!flip){ //rusty turn
             int turn = 0;
             while (turn < k) {
+                if(rustyValues.getMax() == -1){break;}
                 int value = rustyValues.getMax();
                 int index = rustyValues.getMaxIndex();
-                std::cout << value << " " << index << std::endl;
+                //std::cout << value << " " << index << std::endl;
                 rustyValues.removeMax();
                 //if(!(std::find(usedBalls.begin(), usedBalls.end(), index) != usedBalls.end())){
                 if(!used[index]){
                     rustyScore += value;
+                    std::cout << "turn " << turn << "rusty choses " << value <<std::endl;
+
                     turn++;
-                    totalTurns+=1;
+                    totalTurns++;
                     used[index] = true;
 
                 }
-                if(rustyValues.isEmpty()){break;}
+
             }
         }
         flip = !flip;
@@ -200,12 +207,13 @@ int main(int argc, char *argv[]) {
 //1000 197
 //240 150
 //2100000000 98888899
-//9751 2043
-//30105 17722
-//4648186903 4020309125
-//13823 12513
-//2174 1664
+//9538 2256
+//30031 17796
+//4726793900 3941702128
+//13793 12543
+//2173 1665
 //3923529875 3049188235
 //0 284401
-    std::cout << "scores " << scottScore << " " << rustyScore << " " << totalTurns;
+//std::cout << "RustyTurns: " << rustyTurns << " " << "ScottTurns: " << scottTurns << std::endl;
+    std::cout << "scores " << scottScore << " " << rustyScore << " "; //<< totalTurns;
 }
